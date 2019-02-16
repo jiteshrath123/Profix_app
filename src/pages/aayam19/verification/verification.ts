0c5db9500d4dae648828e6aa958ecbf28e3b2b45
jiteshrath123 <jiteshrath123>
2019-02-16 16:35:02 +0530

    yes

--- src/pages/aayam19/verification/verification.ts
+++ src/pages/aayam19/verification/verification.ts
@@ -156,7 +156,7 @@ export class VerificationPage {
                 {
                   text: 'OK!',
                   handler: () => {
-                  }
+                }
                 }
               ]
             });
